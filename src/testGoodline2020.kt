eac93c4f8e47ac424ce2fa4579235fb29e79dbcf
Shapovalova Vera <17ametista17@gmail.com>
2020-02-29 09:03:39 +0700

    Update testGoodline2020.kt

--- src/testGoodline2020.kt
+++ src/testGoodline2020.kt
@@ -1,34 +1,42 @@
 package testGoodline2020
 
-fun main() {
-    level5()
+fun main(args: Array<String>) {
+    val inputArray = {
+            args: Array<String> -> if (args.size != 0) args else readLine()!!.filterNot { v -> "\"".contains(v) }.split(" ").toTypedArray()
+    }
+
+    level5(inputArray(args))
 }
-fun level1(){
-    var stroke = readLine()!!.split(" ")
-        .forEach{println(it)}
+
+fun level1(array: Array<String>) {
+    array.forEach { println(it) }
 }
-fun level2(){
-    var stroke = readLine()!!.split(" ")
+
+fun level2(array: Array<String>) {
+    array
         .sorted()
-        .forEach{println(it)}
+        .forEach { println(it) }
 }
-fun level3(){
-    var stroke = readLine()!!.split(" ")
+
+fun level3(array: Array<String>) {
+    array
         .toSet()
         .sorted()
-        .forEach{println(it)}
+        .forEach { println(it) }
 }
-fun level4(){
-    var stroke = readLine()!!.split(" ")
-        .groupingBy{it}
+
+fun level4(array: Array<String>) {
+    array
+        .groupingBy { it }
         .eachCount()
-        .forEach{println("${it.key} ${it.value}")}
+        .forEach { println("${it.key} ${it.value}") }
 }
-fun level5(){
-    var stroke = readLine()!!.split(" ")
-        .groupingBy{it}
+
+fun level5(array: Array<String>) {
+    array
+        .groupingBy { it }
         .eachCount()
         .toList()
-        .sortedWith(compareBy({-it.second}, {it.first}))
-        .forEach{println("${it.first} ${it.second}")}
+        .sortedWith(compareBy({ -it.second }, { it.first }))
+        .forEach { println("${it.first} ${it.second}") }
 }
